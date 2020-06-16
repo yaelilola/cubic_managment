5b4fe036766f65aef9afdf6ed4f64e75c1684999
yael <yaeliola@gmail.com>
2020-06-16 15:54:04 +0300

    documenting and arranging the aws_experiments script

--- scripts/aws_experiment.py
+++ scripts/aws_experiment.py
@@ -14,11 +14,17 @@ https://dev.to/rhymes/how-to-make-python-code-concurrent-with-3-lines-of-code-2f
 https://stackoverflow.com/questions/15752973/python-how-can-i-send-multiple-http-requests-at-the-same-time-like-fork
 """
 
+
+"We used the first couple of macros for experiments that checked multiple urls"
 # NUM_OF_REQUESTS = 200
 # NUM_OF_URLS_MULTIPLE = 5
 NUM_OF_REQUESTS = 1000
 NUM_OF_URLS_MULTIPLE = 1
 
+
+""" sends a get request to url using session and with ot without proxies.
+  returns a tuple including the request status code, request's response time, request start time, request end time
+  and chosen url. """
 def get_it(url, session, proxies=None):
     time1 = datetime.datetime.now()
     r = session.get(url,proxies=proxies)
@@ -26,6 +32,7 @@ def get_it(url, session, proxies=None):
     return r.status_code, (time2-time1).days/86400.0 + (time2-time1).seconds + (time2-time1).microseconds/1000000, time1, time2, url
 
 
+""" creates concurrent requests to the given url using given session and proxies if needed. """
 def sending_multiple_requests(url, session, proxies=None):
     urls = [url] * NUM_OF_REQUESTS
     with PoolExecutor(max_workers=NUM_OF_REQUESTS * NUM_OF_URLS_MULTIPLE) as executor:
@@ -34,6 +41,9 @@ def sending_multiple_requests(url, session, proxies=None):
         res =executor.map(lambda p: get_it(*p), args)# (*p) does the unpacking part
         return res
 
+
+""" login to the website using a user with both focal point and space planner credits. using proxies if needed
+returns the session for continuing interacting with the site """
 def login(proxies = None):
     session_requests = requests.session()
     login_url = "https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/login/"
@@ -45,6 +55,7 @@ def login(proxies = None):
     return session_requests
 
 
+""" writes the data from url_res_generator to a wanted file (when using it, should change the file name! """
 def write_multiple_requests_to_csv(url_res_generator, ax, color, label):
     response_times = []
     multiple_requests_data = ""
@@ -63,13 +74,8 @@ def write_multiple_requests_to_csv(url_res_generator, ax, color, label):
     f.write(multiple_requests_data)
     f.close()
 
-    # t = np.arange(1., NUM_OF_REQUESTS + 1, 1)
-    # # plt.plot(t, response_times[0:999], 'r-', t, response_times[1000:1999], 'bs-', t, response_times[2000:2999], 'g-',
-    # #          t, response_times[3000:3999], 'c-', t, response_times[4000:4999], 'y-')
-    # ax.plot(t, response_times, color, label=label)
-    # ax.legend()
 
-
+# was used to decide matplot graphs color. not used anymore
 def determine_color_and_label(url):
     if url == 'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/users/mycubic/':
         return 'r-', "my cubic"
@@ -83,15 +89,23 @@ def determine_color_and_label(url):
         return 'y-', "all cubics"
 
 
+""" experiment 1: sending 1000 concurrent get requests to the website. 
+part 1- sending 1000 concurrent requests to the same url:  
+                'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/users/mycubic/'
+part 2- sending 200 concurrent requests to 5 different urls (meaning 5*200 =1000 total). 
+the part two urls are: 
+                'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/users/mycubic/',
+                'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/focal_point/floorstatistics',
+                'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/focal_point/assignments/',
+                'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/space_planner/statistics/campuses/',
+                'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/facilities/cubics/'
+"""
 def exp1(session):
     # get_urls = ['https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/users/mycubic/',
     #             'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/focal_point/floorstatistics',
     #             'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/focal_point/assignments/',
     #             'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/space_planner/statistics/campuses/',
     #             'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/facilities/cubics/']
-    # get_urls = ['https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/facilities/cubics/',
-    #             'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/users/mycubic/',
-    #             'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/focal_point/assignments/']
     get_urls = ['https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/users/mycubic/']
     ax = plt.subplot(111)
     for url in get_urls:
@@ -99,30 +113,9 @@ def exp1(session):
         color, label = determine_color_and_label(url)
         write_multiple_requests_to_csv(res, ax, color, label)
 
-def exp1_c(session):
-    get_urls = ['https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/users/mycubic/']
-    ax = plt.subplot(111)
-    us_ip = "http://104.129.202.148:10103"
-    proxies = {"http": "104.129.202.148:10103", 'https': "104.129.202.148:10103"}
-    for url in get_urls:
-        res = sending_multiple_requests(url, session, proxies)
-        color, label = determine_color_and_label(url)
-        write_multiple_requests_to_csv(res, ax, color, label)
 
-def exp1_d(session):
-    get_urls = ['https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/users/mycubic/',
-                'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/focal_point/floorstatistics',
-                'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/focal_point/assignments/',
-                'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/space_planner/statistics/campuses/',
-                'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/facilities/cubics/']
-    # get_urls = ['https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/users/mycubic/']
-    ax = plt.subplot(111)
-    for url in get_urls:
-        res = sending_multiple_requests(url, session)
-        color, label = determine_color_and_label(url)
-        write_multiple_requests_to_csv(res, ax, color, label)
-
-
+"""getting a single get request to the given url (using session) request status code, request's response time, 
+   request start time, request end time and chosen url.  """
 def exp1_e(session, url):
     res = get_it(url, session)
     row = ""
@@ -135,26 +128,22 @@ def exp1_e(session, url):
     f.close()
 
 
+""" experiment 2: simulating sending get requests from different countries to the website. 
+all requests were to the same url:  'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/users/mycubic/'
+for each given 
+this function receives the session to use and country to simulate requests from, and prints 10 responses details 
+(after sending 10 non concurrent requests)
+"""
 def exp2(session, country):
     i = 0
     while i < 10:
         try:
-            # get_urls = ['https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/users/mycubic/',
-            #             'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/focal_point/floorstatistics',
-            #             'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/focal_point/assignments/',
-            #             'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/space_planner/statistics/campuses/',
-            #             'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/facilities/cubics/']
-            # get_urls = ['https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/facilities/cubics/',
-            #             'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/users/mycubic/',
-            #             'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/focal_point/assignments/']
             get_urls = ['https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/users/mycubic/']
             proxy_orbit_url = "https://api.proxyorbit.com/v1/?token=A1MSGdShFJYc8XtKK-TpuEDqEPLw58yTcgoZA6t9ZFs&location={country}"
-            # presp = requests.get(proxy_orbit_url.format(country=country))
-            # ip = presp.json()['curl']
-            # https_ip = ip.replace('http', 'https')
-            # proxies = {"http": ip, 'https': https_ip}
-            ip = "http://155.138.138.36:8080"
-            proxies = {"http": "http://155.138.138.36:8080", 'https': "http://155.138.138.36:8080"}
+            presp = requests.get(proxy_orbit_url.format(country=country))
+            ip = presp.json()['curl']
+            https_ip = ip.replace('http', 'https')
+            proxies = {"http": ip, 'https': https_ip}
             url = get_urls[0]
 
             time1 = datetime.datetime.now()
@@ -169,9 +158,18 @@ def exp2(session, country):
             print("failed")
             pass
 
+
+""" experiment 3: simulating sending get requests to the website, after different time of the web site being "asleep":
+we checked after 1 minute, 10 minute, one hour and two hours. 
+we sent 10 requests for each time chosen, to the same url- 
+'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/users/mycubic/'
+(for example, after i minute of the website being inactive, we sent a request, then waited 1 more minute and sent again. 
+we continued like this until we had 10 requests to the website after 1 minute of inactivity.)
+the function writes the response details (as in previous experiments) to the matching time file. 
+"""
 def exp3(session):
     get_urls = ['https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/users/mycubic/']
-    times = [60*60*2, 60*60*24]
+    times = [60, 60*10, 60*60, 60*60*2]
     for time in times:
         print("checking now: " + str(time))
         i = 0
@@ -188,25 +186,29 @@ def exp3(session):
             i = i+1
 
 
-
+# we used this main in order to run different experiments.
 def main():
     session = login()
     print("logged in")
-    # exp1(session)
-    #countries = ["CA"]
-    #for country in countries:
-        #exp2(session, country)
-        # sleep(10)
+    # running the first experiment
+    exp1(session)
+    # running part e of the first experiment.
+    get_urls = ['https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/users/mycubic/',
+                'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/focal_point/floorstatistics',
+                'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/focal_point/assignments/',
+                'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/space_planner/statistics/campuses/',
+                'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/facilities/cubics/']
+    for url in get_urls:
+        exp1_e(session, url)
 
-    # plt.title('Multiple Requests To Website')
-    # plt.ylabel('response time(seconds)')
-    # plt.show()
+    # running the second experiment
+    countries = ["CA", "DE", "US", "CY", "PE", "IN", "AU"]
+    for country in countries:
+        exp2(session, country)
+
+    # running the third experiment
     exp3(session)
-    # get_urls_504 =['https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/focal_point/floorstatistics',
-    #             'https://pc6m9n5jrh.execute-api.us-east-2.amazonaws.com/dev/space_planner/statistics/campuses/']
-    # for url in get_urls_504:
-    #     exp1_e(session, url)
-    # exp1_c(session)
+
     return
 
 
